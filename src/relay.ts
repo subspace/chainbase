c7aa4f6631e36d8149d3d4f81b4e620c38cbcb05
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2020-12-13 17:48:26 +0200

    Subscribe to mined bitcoin blocks, fix some linting warnings

--- src/relay.ts
+++ src/relay.ts
@@ -1,5 +1,5 @@
-import { EventEmitter } from 'events';
-// import chosen websocket library
+import {EventEmitter} from 'events';
+import {client, connection} from "websocket";
 
 export interface IBitcoinBlock {
     height: number;
@@ -10,26 +10,65 @@ export interface IBitcoinBlock {
 }
 
 export class Relay extends EventEmitter {
-
-    constructor() {
-        super()
-    }
-
-    /* 
+    /*
      * Connects to Bitcoin.com Websocket API and subscribes to new blocks.
      */
-    public async connectToBTCRelay() {
-        // connect over WS using public endpoint 
-        // let connection;
+    public async connectToBTCRelay(): Promise<void> {
+        const wsClient = new client();
 
-        // // subscribe to new block events
+        const connection = await new Promise(
+            (
+                resolve: (connection: connection) => void,
+                reject: (errorDescription: Error) => void,
+            ) => {
+                wsClient
+                    .on('connect', (connection: connection) => {
+                        resolve(connection);
+                    })
+                    .on('connectFailed', (errorDescription: Error) => {
+                        reject(errorDescription);
+                    })
+                    .connect('wss://ws.blockchain.info/inv');
+            },
+        );
 
-        // // register event handler for new blocks
-        // connection.on("block", (block: any) => {
+        connection.sendUTF(JSON.stringify({op: 'blocks_sub'}));
 
-        //     // parse the json into IBitcoinBlock
-
-        //     this.emit("block", block);
-        // })
+        connection.on('message', (message) => {
+            if (message.utf8Data) {
+                try {
+                    const decoded = JSON.parse(message.utf8Data);
+                    switch (decoded.op) {
+                        case 'block':
+                            if (
+                                typeof decoded.bits === 'number' &&
+                                typeof decoded.hash === 'string' &&
+                                typeof decoded.height === 'number' &&
+                                typeof decoded.mrklRoot === 'string' &&
+                                typeof decoded.nonce === 'number'
+                            ) {
+                                const block: IBitcoinBlock = {
+                                    bits: decoded.bits,
+                                    hash: Buffer.from(decoded.hash, 'hex'),
+                                    height: decoded.height,
+                                    merkleRoot: Buffer.from(decoded.mrklRoot, 'hex'),
+                                    nonce: decoded.nonce,
+                                };
+                                console.info('New bitcoin block received', block);
+                                this.emit("block", block);
+                            } else {
+                                console.error('Unexpected block structure from blockchain.info', decoded);
+                            }
+                            break;
+                        default:
+                            console.error('Unexpected operation from blockchain.info', decoded);
+                    }
+                } catch (e) {
+                    console.error('Failed to decode JSON message from blockchain.info', e);
+                }
+            } else {
+                console.error('Received unexpected binary message from blockchain.info');
+            }
+        });
     }
 }
